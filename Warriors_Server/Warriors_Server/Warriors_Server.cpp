0b6fd7bd50eafb22f8c462015154ecb60758b59a
BBaKKi <dodpgh00@naver.com>
2021-09-29 20:59:30 +0900

    Find Movement Error

--- Warriors_Server/Warriors_Server/Warriors_Server.cpp
+++ Warriors_Server/Warriors_Server/Warriors_Server.cpp
@@ -153,8 +153,6 @@ void send_tick_packet(int p_id)
 	packet.enemy_guard_hit = enemy_player.m_guard_hit;
 
 	send_packet(p_id, &packet, SERVER_TICK);
-
-	cout << packet.player_stamina << endl;
 }
 
 void do_recv(int p_id)
@@ -196,7 +194,11 @@ void process_packet_change_character(int p_id, client_packet_change_character* p
 void process_packet_reduce_stamina(int p_id, client_packet_reduce_stamina* packet)
 {
 	//players[p_id].m_lock.lock();
-	if((players[p_id].m_stamina - packet->reduce_stamina) >= 0.f)
+	if(players[p_id].m_stamina >= 1.f)
+	{
+		players[p_id].m_stamina = 1.f;
+	}
+	if(((players[p_id].m_stamina) - (packet->reduce_stamina)) >= 0.f)
 	{
 		players[p_id].m_stamina -= packet->reduce_stamina;
 	}
